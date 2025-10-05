bf7e846b0519b0e4cba363942305554962d24e71
Juan Esteban Moncada Bonilla <juanesmoncada123@gmail.com>
2025-10-05 16:41:12 -0700

    Deleting unnecessary env vars

--- internal/app/configs.go
+++ internal/app/configs.go
@@ -17,8 +17,8 @@ func readConfigs() Configs {
 	linkedInConfigs := LinkedInConfigs{
 		AccessToken: os.Getenv("LINKEDIN_ACCESS_TOKEN"),
 		AccountID:   os.Getenv("LINKEDIN_ACCOUNT_ID"),
-		BaseURL:     os.Getenv("LINKEDIN_BASE_URL"),
-		Version:     os.Getenv("LINKEDIN_VERSION"),
+		BaseURL:     "https://api.linkedin.com/rest",
+		Version:     "202505",
 	}
 
 	return Configs{
